6dfe107fd26d1c1aca95ff139c50ba1936db639a
Nathaniel Villegas <nathanielvillegas@Nathaniels-MBP.att.net>
2017-08-08 22:12:57 -0700

    functioning

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('All-American');
+INSERT INTO burgers (burger_name) VALUES ('Hawaiian');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Chicken', true);
+INSERT INTO burgers (burger_name) VALUES ('Veggie');
